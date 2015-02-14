55fd08e70257bef5665c87fc4532d038932aa41c
Daniel Phillips <dphildev@gmail.com>
2015-02-14 19:02:55 +0000

    The start of a build script for the Mac. You'll need Qt in your path for this to work.
    The script will:
    * Build the application in release.
    * Package Qt dependencies into the .app.
    * Create a .dmg containing the application.
    
    Still need to improve it and check return codes though. I'm also not too sure about the .app file renaming- will see how it goes.

---
+++ macbuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir build
+cd build
+qmake "CONFIG+=release" ../main.pro
+make
+mv busalert/busalert.app/Contents/MacOS/busalert "busalert/busalert.app/Contents/MacOS/Edinburgh Buses"
+mv busalert/busalert.app "busalert/Edinburgh Buses.app"
+cd busalert
+macdeployqt "Edinburgh Buses.app" -dmg
